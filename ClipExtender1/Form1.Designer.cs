ccd7c5ccd15eb409dd05c8fdd479c4cbbeef0c45
cstilstra <cstilstra@2694ff3c-5118-4684-8680-f386a17b5cba>
2011-12-09 17:57:06 +0000

    Added menustrip and functionality to support it. Cleaned up GUI to represent current functionality.

--- ClipExtender1/Form1.Designer.cs
+++ ClipExtender1/Form1.Designer.cs
@@ -1,4 +1,20 @@
-﻿namespace ClipExtender
+﻿//This file is part of ClipExtender.
+
+//ClipExtender is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//ClipExtender is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
+namespace ClipExtender
 {
     partial class Form1
     {
@@ -30,12 +46,17 @@
         {
             this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.tbEntry = new System.Windows.Forms.TextBox();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnFrom = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutClipExtenderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            this.aboutClipExtenderToolStripMenuItem.Click += aboutClipExtenderToolStripMenuItem_Click;
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -43,43 +64,16 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(13, 32);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(588, 186);
+            this.listBox1.Size = new System.Drawing.Size(277, 251);
             this.listBox1.TabIndex = 0;
             this.listBox1.TabStop = false;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // tbEntry
-            // 
-            this.tbEntry.Location = new System.Drawing.Point(13, 230);
-            this.tbEntry.Name = "tbEntry";
-            this.tbEntry.Size = new System.Drawing.Size(474, 20);
-            this.tbEntry.TabIndex = 1;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(494, 230);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(107, 23);
-            this.btnAdd.TabIndex = 2;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnFrom
-            // 
-            this.btnFrom.Location = new System.Drawing.Point(494, 262);
-            this.btnFrom.Name = "btnFrom";
-            this.btnFrom.Size = new System.Drawing.Size(107, 23);
-            this.btnFrom.TabIndex = 3;
-            this.btnFrom.Text = "Pull from Clipboard";
-            this.btnFrom.UseVisualStyleBackColor = true;
-            this.btnFrom.Click += new System.EventHandler(this.btnFrom_Click);
-            // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(227, 262);
+            this.btnRemove.Location = new System.Drawing.Point(12, 287);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(161, 23);
+            this.btnRemove.Size = new System.Drawing.Size(278, 23);
             this.btnRemove.TabIndex = 4;
             this.btnRemove.Text = "Remove from ClipExtender";
             this.btnRemove.UseVisualStyleBackColor = true;
@@ -87,9 +81,9 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(13, 262);
+            this.btnClear.Location = new System.Drawing.Point(12, 316);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(107, 23);
+            this.btnClear.Size = new System.Drawing.Size(278, 23);
             this.btnClear.TabIndex = 5;
             this.btnClear.Text = "Clear ClipExtender";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -99,21 +93,60 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(306, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutClipExtenderToolStripMenuItem});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(44, 20);
+            this.menuHelp.Text = "Help";
+            // 
+            // aboutClipExtenderToolStripMenuItem
+            // 
+            this.aboutClipExtenderToolStripMenuItem.Name = "aboutClipExtenderToolStripMenuItem";
+            this.aboutClipExtenderToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.aboutClipExtenderToolStripMenuItem.Text = "About ClipExtender";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 20);
+            this.menuFile.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
             // Form1
             // 
-            this.AcceptButton = this.btnAdd;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 297);
+            this.ClientSize = new System.Drawing.Size(306, 351);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnRemove);
-            this.Controls.Add(this.btnFrom);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.tbEntry);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ClipExtender";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,12 +155,14 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox tbEntry;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnFrom;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp;
+        private System.Windows.Forms.ToolStripMenuItem aboutClipExtenderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
