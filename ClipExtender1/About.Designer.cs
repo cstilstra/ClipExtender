792ec7108def4315329214fc02fd1fcf0bf6f355
cstilstra <cstilstra@gmail.com>
2016-04-07 16:22:45 -0400

    moved clipboard communication into own class

--- ClipExtender1/About.Designer.cs
+++ ClipExtender1/About.Designer.cs
@@ -35,16 +35,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.MaximumSize = new System.Drawing.Size(275, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.Size = new System.Drawing.Size(250, 78);
             this.label1.TabIndex = 0;
-            this.label1.Text = "This is the \'About\' page";
+            this.label1.Text = "An extension of the Windows clipboard allowing for multiple selections to be stor" +
+    "ed simultaneously.\r\n\r\nLicensed under GNU GPLv3\r\n\r\nDeveloped by c.s.tilstra\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 113);
             this.Controls.Add(this.label1);
             this.Name = "About";
             this.Text = "About";
--- ClipExtender1/Form1.cs
+++ ClipExtender1/Form1.cs
@@ -14,8 +14,6 @@
 //along with ClipExtender.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.IO;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace ClipExtender
@@ -24,149 +22,52 @@ namespace ClipExtender
     {
 
         //used to determine if a clipboard change has already been acted upon
-        Boolean hasRunOnce = false;
+        public Boolean messageHasBeenProcessed = false;
 
         //the path that the application executable is installed to
         String appPath = Application.StartupPath;
 
-        //required for sub/unsub to the clipboard listener list
-        [DllImport("user32.dll")]
-        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
-        [DllImport("user32.dll")]
-        public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
-
-        //required to recognize clipboard update messages
-        private static int WM_CLIPBOARDUPDATE = 0x031D;
-
+        ClipboardCommunication clipboardCommunication;
        
         public Form1()
         {
             InitializeComponent();
-            selectLastItem();           
+            setUp();
+        }
 
-            //if we cannot add this window as a format listener show a message box and abort
-            if (!AddClipboardFormatListener(this.Handle))
-            {
-                MessageBox.Show("Failed to add clipboard format listener, closing program.");
-                Application.Exit();
-            }
+        //sets up references and starting state
+        private void setUp()
+        {
+            selectLastItem();
+            clipboardCommunication = new ClipboardCommunication(this);
+            clipboardCommunication.beginListeningToClipboard(this.Handle);
         }
 
         //selects the last item in the list
-        private void selectLastItem()
+        public void selectLastItem()
         {
-            //variable to hold the number of items in the list
             int numberOfItems = listBox1.Items.Count;
-            //variable to hold the index number of the last item in the list
-            int lastItem = numberOfItems - 1;
-            //if we have entries in the list, select the last one
+            int lastItemIndex = numberOfItems - 1;
             if (numberOfItems > 0)
-                listBox1.SetSelected(lastItem, true);
-        }
-
-        //selects the next item in the list after the currently selected item
-        private void selectNextItem(int currentSelectedIndex)
-        {
-            //variable to hold the number of items in the list
-            int numOfEntries = listBox1.Items.Count;
-
-            //if we have entries in the list, select the next entry
-            if (numOfEntries > 0)
             {
-                //if the selected entry was the last in the list
-                if (numOfEntries == currentSelectedIndex)
-                {
-                    //take one from currentSelectedIndex in order to avoid error
-                    currentSelectedIndex--;
-                }
-                listBox1.SetSelected(currentSelectedIndex, true);
+                listBox1.SetSelected(lastItemIndex, true);
             }
         }
 
         //handles the message that is sent when the clipboard updates
         protected override void DefWndProc(ref Message m)
         {
-            if (m.Msg == WM_CLIPBOARDUPDATE)
-            {
-                //stops clipExtender from putting itself into an infinite loop every time something is copied
-                if (hasRunOnce == false)
-                {
-                    //changes the clipboard contents, which resends the clipboard update message
-                    //which reruns this subroutine, etc, looping forever without a toggle variable
-                    getClipboardData();
-                }
-                //hasRunOnce will only ever be true within the first 100ms of having copied something
-                //because we are not able to react to the clipboard update message within this time
-                //it stops the program from continually looping
-                else if (hasRunOnce == true) { }
-            }
-            else
+            //if the clipboard extension doesn't need the messge
+            if (!clipboardCommunication.handleUpdateMessage(m))
             {
                 base.DefWndProc(ref m);
-            }
-        }
+            }            
+        }        
 
-        //this will pull the contents of the clipboard and add to the listbox
-        private void getClipboardData()
-        {
-            //pull the text contents of the clipboard
-            //if no text content exists, this will return a null value
-            String textFromClipboard = pullFromClipboard();
-            ////declare a boolean and populate it the results of testing if textFromClip is found in the listbox
-            ////this is currently useless since the code resulting from this test is non functional
-            //Boolean textAlreadyInList = findString(textFromClip);
-
-            //if the clipboard does not contain text
-            if (textFromClipboard == null)
-            {
-                //then show us a message box saying so
-                MessageBox.Show("The Clipboard does not contain text at this time.");
-
-                //otherwise (if the clipboard does contain text)
-            }
-            else {
-                //if the string is not already in the listbox add it
-                if (!findStringInList(textFromClipboard))
-                {                    
-                    listBox1.Items.Add(textFromClipboard);
-                }
-            }
-
-            //set runOnce to true so that the clipboard change message will not be acted upon again
-            //which would result in an endless loop
-            hasRunOnce = true;
-
-            //select the last item in the list, which causes sending of the clipboard change message
-            selectLastItem();
-            //start the timer
-            //once the timer has ticked, runOnce will be set back to false
-            //enabling the clipboard change message to be acted upon again
-            timer1.Start();
-            //}
-
-        }
-
-        //checks if the clipboard currently holds text, if so pulls that text and returns it as a string
-        private string pullFromClipboard()
-        {
-            //create the string that will potentially hold the clipboard text
-            String returnHtmlText = null;
-            Boolean clipBoardContainsText = Clipboard.ContainsText();
-            //test if the clipboard containts text
-            if (clipBoardContainsText == true)
-            {
-                //pull text 
-                returnHtmlText = Clipboard.GetText();
-            }
-
-            return returnHtmlText;
-        }
-
-        //determines if a string already exists as an item in the listbox and returns a boolean value
-        private bool findStringInList(string searchString)
+        //determines if a string already exists as an item in the listbox
+        public bool findStringInList(string searchString)
         {
             int index = listBox1.FindStringExact(searchString);
-            //if index is NOT equal to listbox.nomatches = if there is a match
             if (index == ListBox.NoMatches)
                 return false;
             else
@@ -176,17 +77,14 @@ namespace ClipExtender
         //handles the selection changing in the listbox
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //create an instance of the selected item (this would be the item after the selection has changed)
-            object item = listBox1.SelectedItem;
-            //pull the text from that instanced object
-            string toClipboard = listBox1.GetItemText(item);
-            hasRunOnce = true;
+            //get the text from the newly selected item
+            string toClipboard = listBox1.GetItemText(listBox1.SelectedItem);
+            //indicate that the message has been processed
+            messageHasBeenProcessed = true;
             //send it to the clipboard
             Clipboard.SetDataObject(toClipboard);
-            //start the timer
-            //once the timer has ticked, runOnce will be set back to false
-            //enabling the clipboard change message to be acted upon again
-            timer1.Start();
+            //start the timer that will reset messageHasBeenProcessed to false
+            hasRunOnceTimer.Start();
         }
 
         //clears the listbox and clipboard
@@ -199,18 +97,33 @@ namespace ClipExtender
         //removes the current selection from the listbox and sets the seletion to the next item
         private void btnRemove_Click(object sender, EventArgs e)
         {
-            int currentSelection = listBox1.SelectedIndex;
+            int currentSelectionIndex = listBox1.SelectedIndex;
             listBox1.Items.Remove(listBox1.SelectedItem);
-            selectNextItem(currentSelection);
-        } 
+            selectNextItemAfterDeletion(currentSelectionIndex);
+        }
 
-        //opens the "About" form
+        //selects the next item in the list after the just deleted item
+        private void selectNextItemAfterDeletion(int deletedIndex)
+        {
+            int numberOfItems = listBox1.Items.Count;
+            if (numberOfItems > 0)
+            {
+                //if the deleted entry was the last in the list
+                if (numberOfItems == deletedIndex)
+                {
+                    //decrement deletedIndex to select the new last item 
+                    deletedIndex--;
+                }
+                listBox1.SetSelected(deletedIndex, true);
+            }
+        }
+
         private void aboutClipExtenderToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            //About AboutFRM = new About();
-            Form1 ParentForm = new Form1();
-            //AboutFRM.StartPosition = ParentForm.StartPosition;
-            //AboutFRM.Show();
+            About AboutFRM = new About();
+            Form1 ParentForm = this;
+            AboutFRM.StartPosition = ParentForm.StartPosition;
+            AboutFRM.Show();
         }
 
         //exits the program
@@ -218,20 +131,16 @@ namespace ClipExtender
         {
             Application.Exit();
         }
-
-        //this timer will be used to reset the runOnce variable to false
-        //because this timer ticks at an interval of 100ms
-        //it is not possible to copy something within 100ms of having copied before
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-            hasRunOnce = false;
+            messageHasBeenProcessed = false;
         }
-
-        //when we close the form, remove it as a clipboard monitor
+        
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            RemoveClipboardFormatListener(this.Handle);
-            Directory.Delete(appPath + "Temp");
+            //remove form as clipboard listener
+            clipboardCommunication.endListeningToClipBoard(this.Handle);
         }
     }
 }
